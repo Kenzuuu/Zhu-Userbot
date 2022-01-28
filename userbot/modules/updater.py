13a4445d90cf8dc6dba850b44df94e615e19cec9
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-01-28 17:33:25 +0800

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -220,7 +220,7 @@ async def upstream(event):
         else:
             await event.edit(changelog_str)
         return await event.respond(
-            "**Perintah Untuk Update, Sebagai Berikut.**\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`.update now`\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`.update deploy`\n\nUntuk Meng Update Fitur Terbaru Dari **𝐒𝐊𝐘𝐙𝐔-𝐔𝐒𝐄𝐑𝐁𝐎𝐓​**"
+            "**Perintah Untuk Update, Sebagai Berikut.**\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`.update now`\n 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`.update deploy`\n\nUntuk Meng Update Fitur Terbaru Dari **💘L̷O̷V̷E̷-̷U̷S̷E̷R̷B̷O̷T̷💘**"
         )
 
     if force_update:
