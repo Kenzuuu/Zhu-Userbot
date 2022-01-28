0a07faac81956d95bae8752853803587880d574a
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-01-28 20:19:00 +0800

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -182,7 +182,7 @@ async def redis(pong):
 
 
 @register(outgoing=True, pattern="^.ping$")
-@register(incoming=True, from_users=DEV, pattern=r"^.cping$")
+@register(incoming=True, from_users=DEVS, pattern=r"^.cping$")
 async def redis(pong):
     """For .ping command, ping the userbot from any chat."""
     uptime = await get_readable_time((time.time() - StartTime))
