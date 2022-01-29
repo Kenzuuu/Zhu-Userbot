fe643b8af1eb0069992208517bf84c03a36e39ed
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-01-29 09:43:59 +0800

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -191,10 +191,10 @@ async def redis(pong):
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await pong.edit(
-        f"**`{ALIVE_NAME}`**\n\n
-        f"**Pong !!** `%sms` \n"
-        f"** Uptime ** - `{uptime}`\n % (duration)
-    )
+                   f"**`{ALIVE_NAME}` Ping !!**\n\n" 
+                   f"**Pong !!** `%sms` \n"
+                   f"** Uptime ** - `{uptime}`\n % (duration)
+                   )
 
 
 @register(outgoing=True, pattern="^.kecepatan$")
