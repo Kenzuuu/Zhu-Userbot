c9d356fccca40da13842dc9abc458ded9935efc1
Kenzuuu <alifalansyah485@gmail.com>
2022-01-31 10:42:58 +0000

    pylint: auto fixes
    Signed-off-by: Kenzuuu <alifalansyah485@gmail.com>

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -352,7 +352,7 @@ async def spider(spdr):
     # If everything goes well, do announcing and mute
     await spdr.edit(
         f"[{user.first_name}](tg://user?id={user.id}) 🔇 Anda telah Dibisukan dari group ini\n"
-       )
+    )
     await sleep(20)
     if mute(spdr.chat_id, user.id) is False:
         return await spdr.edit(
