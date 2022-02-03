ce3e0f1205aabd31eb00a72f8fbcbad753f39a0b
Kenzuuu <alifalansyah485@gmail.com>
2022-02-03 23:33:08 +0000

    pylint: auto fixes
    Signed-off-by: Kenzuuu <alifalansyah485@gmail.com>

--- userbot/modules/aatesting.py
+++ userbot/modules/aatesting.py
@@ -3,14 +3,12 @@
     Information Superhighway (yes, Internet). """
 
 import asyncio
-import random
 import time
 from datetime import datetime
 
 import redis
-from speedtest import Speedtest
 
-from userbot import ALIVE_NAME, CMD_HELP, DEVS, StartTime
+from userbot import DEVS, StartTime
 from userbot.events import register
 
 LOVE_IMG = os.environ.get(
