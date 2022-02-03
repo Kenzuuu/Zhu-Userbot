1ff69fa7f51070855492da461fc7c95312fdc09a
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-02-04 07:29:40 +0800

    Create aatesting.py

--- userbot/modules/aatesting.py
+++ userbot/modules/aatesting.py
@@ -1,3 +1,16 @@
-# Credit to @triplenineee
+#Credit to @triplenineee
 
+import asyncio
+import random
+import time
+from datetime import datetime
 
+import redis
+from speedtest import Speedtest
+
+from userbot import ALIVE_NAME, CMD_HELP, DEVS, StartTime
+from userbot.events import register
+
+LOVE_IMG = os.environ.get(
+    "PING_PIC", "https://te.legra.ph/file/a59da36828333262c9848.jpg"
+)
