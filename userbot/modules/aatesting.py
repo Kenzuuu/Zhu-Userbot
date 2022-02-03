e33b41a4a2b31257d8943e20eaa124046ac5a56b
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-02-04 07:31:15 +0800

    Update aatesting.py

--- userbot/modules/aatesting.py
+++ userbot/modules/aatesting.py
@@ -6,3 +6,19 @@
 LOVE_IMG = os.environ.get(
     "PING_PIC", "https://te.legra.ph/file/a59da36828333262c9848.jpg"
 )
+
+
+@register(outgoing=True, pattern="^.ping$")
+@register(incoming=True, from_users=DEVS, pattern=r"^.cping$")
+async def redis(pong):
+    """For .ping command, ping the userbot from any chat."""
+    uptime = await get_readable_time((time.time() - StartTime))
+    start = datetime.now()
+    await pong.edit("💝")
+    await asyncio.sleep(3)
+    end = datetime.now()
+    duration = (end - start).microseconds / 1000
+    await pong.edit(
+        f"**Pong !!** `%sms` \n"
+        f"**Uptime **- `{uptime}`\n" % (duration)
+    )
