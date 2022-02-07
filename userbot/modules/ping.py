0fa389db975731dc1a6055e1f616c35f6aea647e
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-02-08 05:01:58 +0800

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -5,6 +5,10 @@
 """ Userbot module containing commands related to the \
     Information Superhighway (yes, Internet). """
 
+import asyncio
+import random
+import time
+from datetime import datetime
 
 async def get_readable_time(seconds: int) -> str:
     count = 0
