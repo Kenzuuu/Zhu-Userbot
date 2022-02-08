522c26ee789f0540f87e20a2b3a58e555831ef05
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-02-08 08:51:31 +0800

    Update fban.py

--- userbot/modules/fban.py
+++ userbot/modules/fban.py
@@ -28,7 +28,7 @@ async def fban(event):
 
     if event.sender_id == fban_id:
         return await event.edit(
-            "**Error: This action has been prevented by KensurBot self preservation protocols.**"
+            "**Error: This action has been prevented by Shinobi Team self preservation protocols.**"
         )
 
     if len((fed_list := get_flist())) == 0:
