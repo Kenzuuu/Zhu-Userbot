3c762efd39fb5c8685e7057ceae59c2bb9581798
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-02-10 09:12:05 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -128,7 +128,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL", "https://github.com/Kenzuuu/Zhu-Userbot"
+    "UPSTREAM_REPO_URL", "https://github.com/Kenzuuu/Zhu"
 )
 UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Kenzhu")
 
