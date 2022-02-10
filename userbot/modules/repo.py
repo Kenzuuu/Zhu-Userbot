89683d106e4bc5af9c8768043a4032c9c6aee621
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-02-10 08:24:48 +0800

    Update repo.py

--- userbot/modules/repo.py
+++ userbot/modules/repo.py
@@ -66,15 +66,7 @@ async def get_readable_time(seconds: int) -> str:
 
     return up_time
 
-@register(outgoing=True, pattern="^.repo$")
-async def repo_is_here(wannasee):
-    """For .repo command, just returns the repo URL."""
-    await wannasee.edit(
-        f"**REPOSITORY**\n"
-        f"💞 **[[[R̸E̸P̸O̸​](https://github.com/Kenzuuu/Zhu-Userbot)]]** 💞 **[[[O̸W̸N̸E̸R̸](t.me/triplenineee)]]** 💞\n"
-    )
-
-@register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
+@register(outgoing=True, pattern=r"^\.repo$")
 async def redis(alive):
     user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
