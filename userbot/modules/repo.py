a0ec14f9b398ea37e0cec3efe70a51ac3eeffe9a
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-02-10 08:18:17 +0800

    Update repo.py

--- userbot/modules/repo.py
+++ userbot/modules/repo.py
@@ -1,6 +1,70 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+# Upload by Kenzuuu/Zhu-Userbot
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot module for getting information about the server. """
 
 
+import asyncio
+import platform
+import sys
+import time
+from asyncio import create_subprocess_exec as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+from datetime import datetime
+from os import remove
+from platform import python_version, uname
+from shutil import which
 
+import psutil
+from telethon import __version__, version
+
+from userbot import (
+    ALIVE_LOGO,
+    ALIVE_NAME,
+    BOT_VER,
+    CMD_HELP,
+    UPSTREAM_REPO_BRANCH,
+    StartTime,
+    bot,
+)
+from userbot.events import register
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+
+modules = CMD_HELP
+
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["Dtk", "Mnt", "Jam", "Hari"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(
+            seconds, 60) if count < 3 else divmod(
+            seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
 
 @register(outgoing=True, pattern="^.repo$")
 async def repo_is_here(wannasee):
