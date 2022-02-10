7a79f651e48adbb9c654205f77d88ff3b81cf422
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-02-10 09:05:30 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -233,6 +233,10 @@ S_PACK_NAME = os.environ.get("S_PACK_NAME", None)
 ALIVE_LOGO = (os.environ.get("ALIVE_LOGO")
               or "https://telegra.ph/file/111c4fcb06e9dee2a9c75.jpg")
 
+# Default .repo
+REPO_LOGO = (os.environ.get(
+      "REPO_LOGO", "https://telegra.ph/file/bab9f4fbc7b1412e674df.jpg"
+
 # Default .helpme Logo
 INLINE_PIC = (os.environ.get("INLINE_PIC")
               or "https://telegra.ph/file/111c4fcb06e9dee2a9c75.jpg")
