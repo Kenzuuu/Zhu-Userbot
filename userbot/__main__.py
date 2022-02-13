1bc5382cca83ccb98884ad5a03ad39f5c6129872
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-02-13 18:40:30 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,34 +5,37 @@
 #
 """ Userbot start point """
 
+import sys
 from importlib import import_module
-from sys import argv
 
-from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import BOT_VER, LOGS, bot
+from userbot import ALIVE_NAME, BOT_VER, BOTLOG_CHATID, LOGS, UPSTREAM_REPO_BRANCH, bot
 from userbot.modules import ALL_MODULES
-
-INVALID_PH = (
-    "\nERROR: The Phone No. entered is INVALID"
-    "\n Tip: Use Country Code along with number."
-    "\n or check your phone number and try again !"
-)
+from userbot.utils.tools import ya_kali_ngga
 
 try:
+    for module_name in ALL_MODULES:
+        imported_module = import_module("userbot.modules." + module_name)
     bot.start()
-except PhoneNumberInvalidError:
-    print(INVALID_PH)
-    exit(1)
-
-for module_name in ALL_MODULES:
-    imported_module = import_module("userbot.modules." + module_name)
+    LOGS.info(f"⛔ Zhu-Userbot ⛔ ⚙️ V{BOT_VER} [ TELAH DIAKTIFKAN! ]")
+except BaseException as e:
+    LOGS.info(str(e), exc_info=True)
+    sys.exit(1)
 
 
-LOGS.info(
-    f"import Zhu modules \n import all modules \n ⛔ZHU-USERBOT⛔ \n ⚙️ VersiBot : V{BOT_VER} \n 🔰 Status : Aktif ✅")
+async def userbot_on():
+    try:
+        if BOTLOG_CHATID != 0:
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"**⚙️ Zhu-Userbot Berhasil Diaktfikan**,
+            )
+    except Exception as e:
+        LOGS.info(str(e))
 
 
-if len(argv) not in (1, 3, 4):
+bot.loop.run_until_complete(userbot_on())
+bot.loop.run_until_complete(ya_kali_ngga())
+if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
