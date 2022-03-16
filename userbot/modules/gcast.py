81ef19cc94057668bb40a877e719208aec08960b
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-03-16 21:55:57 +0800

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -27,7 +27,7 @@ GCAST_BLACKLIST = [
     -1001109837870,  # TelegramBotIndonesia
     -1001752592753,  # Skyzusupport
     -1001736842222,  # Kenzusupport
-    -1001692751821,  # Kyysupport
+    -1001380293847,  # Kyysupport
     -1001286943203,  # StaryGloss
 ]
 
