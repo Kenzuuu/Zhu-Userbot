47582e07aac5d05f8640df2d9fab09771783d7e0
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-03-20 07:44:38 +0800

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -22,3 +22,37 @@ from userbot import (
     bot,
 )
 from userbot.events import register
+
+
+
+
+@register(outgoing=True, pattern=r"^\.(?:alive)\s?(.)?")
+async def amireallyalive(alive):
+    await bot.get_me()
+    await get_readable_time((time.time() - StartTime))
+    output = (
+        f"• ᴏᴡɴᴇʀ : [Zhu](t.me/triplenineee)             ㅤ \n"
+        f"• ꜱʏꜱᴛᴇᴍ. : Ubuntu 20.10            \n"
+        f"• ᴛᴇʟᴇᴛʜᴏɴ : v.{version.__version__}                ㅤㅤ  \n"
+        f"• ᴘʏᴛʜᴏɴ. : v.{python_version()} ㅤㅤ\n"
+        f"• ʙᴏᴛ : v.{BOT_VER}                ㅤㅤㅤ \n"
+        f"• ᴍᴏᴅᴜʟᴇ : {len(modules)} ㅤㅤㅤㅤㅤㅤㅤ   \n"
+    )
+    if ALIVE_LOGO:
+        try:
+            logo = ALIVE_LOGO
+            await alive.delete()
+            msg = await bot.send_file(alive.chat_id, logo, caption=output)
+            await asyncio.sleep(200)
+            await msg.delete()
+        except BaseException:
+            await alive.edit(
+                output + "\n\n *`The provided logo is invalid."
+                "\nMake sure the link is directed to the logo picture`"
+            )
+            await asyncio.sleep(100)
+            await alive.delete()
+    else:
+        await alive.edit(output)
+        await asyncio.sleep(100)
+        await alive.delete()
