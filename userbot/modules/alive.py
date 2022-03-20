96761fead7e40a2f8f672b02d4910b380773a8f5
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-03-20 08:03:54 +0800

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -1,3 +1,5 @@
+""" Userbot module for getting information about the server. """
+
 import asyncio
 import platform
 import sys
@@ -25,6 +27,36 @@ from userbot.events import register
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
+
+modules = CMD_HELP
+
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["Dtk", "Mnt", "Jam", "Hari"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(
+            seconds, 60) if count < 3 else divmod(
+            seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
 
 @register(outgoing=True, pattern=r"^\.(?:alive)\s?(.)?")
 async def amireallyalive(alive):
