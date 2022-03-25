9027ebd7274f5446602db4e39bd81f49ca2f18f2
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-03-25 19:25:23 +0800

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -12,8 +12,8 @@ async def startupmessage():
             await tgbot.send_file(
                 BOTLOG_CHATID,
                 "https://telegra.ph/file/02f87cca391f9b9d627d5.jpg",
-                caption="✨ **Kyy Userbot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @NastyProject ",
-                buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/NastySupportt"),)],
+                caption="💢 **𝗭𝗛𝗨-𝗨𝗦𝗘𝗥𝗕𝗢𝗧 Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 1.1 ©Zhu\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @TripleNineee ",
+                buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/Kenzusupport"),)],
             )
     except Exception as e:
         LOGS.error(e)
