d15177796f7489850b74c55d15cdd7f829a48fef
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-04-13 18:42:09 +0800

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -2,18 +2,19 @@ import logging
 
 from userbot import BOT_USERNAME
 from userbot.events import register
+from userbot.utils import edit_or_reply, Zhu_cmd
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
     level=logging.WARNING)
 
 
-@register(outgoing=True, pattern=r"^\.helpme")
+@Zhu_cmd(pattern="helpme( (.*)|$)")
 async def yardim(event):
     try:
         tgbotusername = BOT_USERNAME
         if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@IdNyaKosong")
+            results = await event.client.inline_query(tgbotusername, "@TripleNineee")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
