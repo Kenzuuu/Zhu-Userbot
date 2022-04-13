1b6e00a1cd9376c5739f130ad99739c2c8dd4255
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-04-13 18:37:01 +0800

    Add files via upload

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -1,48 +1,28 @@
-#    TeleBot - UserBot
-#    Copyright (C) 2020 TeleBot
+import logging
 
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+from userbot import BOT_USERNAME
+from userbot.events import register
 
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-#    Recode by Fariz <Github.com/farizjs>
-#    From Flicks-Userbot
-#    <t.me/TheFlicksUserbot>
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.WARNING)
 
 
-from userbot import BOT_USERNAME, CMD_HELP, bot
-from userbot.utils import edit_or_reply, edit_delete, Zhu_cmd
-
-user = bot.get_me()
-DEFAULTUSER = user.first_name
-CUSTOM_HELP_EMOJI = "✯"
-
-
-@Zhu_cmd(pattern="help ?(.*)")
-async def cmd_list(event):
-    args = event.pattern_match.group(1).lower()
-    if args:
-        if args in CMD_HELP:
-            await edit_or_reply(event, f"**⬣ Cᴏᴍᴍᴀɴ Fᴏʀ {args} ** \n\n" + str(CMD_HELP[args]) + "\n\n**🌐 @Kenzusupport**")
-        else:
-            await edit_delete(event, f"**Module** `{args}` **Tidak tersedia!**")
-    else:
-        try:
-            results = await bot.inline_query(  # pylint:disable=E0602
-                BOT_USERNAME, "@ZhuUserbot"
-            )
+@register(outgoing=True, pattern=r"^\.helpme")
+async def yardim(event):
+    try:
+        tgbotusername = BOT_USERNAME
+        if tgbotusername is not None:
+            results = await event.client.inline_query(tgbotusername, "@IdNyaKosong")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
-        except BaseException:
-            await edit_delete(event, f"** Sepertinya obrolan atau bot ini tidak mendukung inline mode. Untuk Mengaktifkanya silahkan ketik .helpme**")
+        else:
+            await event.edit(
+                "`🔐 Can't Find Your Bot, Please go to @botfather`"
+            )
+    except Exception:
+        return await event.edit(
+            "`🔐 Error, Please send code .inlineon`"
+        )
